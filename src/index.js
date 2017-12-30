6a9f9089c34b215d362f36eaac0e6a8dc0be976b
Andrey Pindyuk <a913000@gmail.com>
2017-12-30 19:54:05 +0700

    Add second screen

--- src/index.js
+++ src/index.js
@@ -12,14 +12,22 @@ const bot = new TelegramBot(config.TOKEN, {
 
 bot.on('message', msg => {
   console.log('Working', msg.from.first_name);
-
+	const chatId = helper.getChatId(msg);
 
 	switch (msg.text) {
 		case kb.home.favourite:
 			break;
 		case kb.home.films:
+			bot.sendMessage(chatId, `Выберите жанр:`, {
+				reply_markup: {keyboard: keyboard.films}
+			});
 			break;
 		case kb.home.cinemas:
+			break;
+		case kb.back:
+			bot.sendMessage(chatId, `Что хотите посмотреть?`, {
+				reply_markup: {keyboard: keyboard.home}
+			});
 			break;
 	}
 });
