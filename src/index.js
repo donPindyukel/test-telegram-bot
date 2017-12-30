39e3d1e2f001b237fba9a88e9f34d13611238739
Andrey Pindyuk <a913000@gmail.com>
2017-12-30 21:16:33 +0700

    Add inline keyboard on film capture

--- src/index.js
+++ src/index.js
@@ -67,6 +67,41 @@ bot.onText(/\/start/, msg => {
 
 });
 
+bot.onText(/\/f(.+)/, (msg, [source, match]) => {
+	const filmUuid = helper.getItemUuid(source);
+	const chatId = helper.getChatId(msg);
+
+	Film.findOne({uuid: filmUuid}).then(film => {
+
+		const caption = `Название: ${film.name}\nГод: ${film.year}\nРейтинг: ${film.rate}\nДлительность: ${film.length}\nСтрана: ${film.country}`;
+
+		bot.sendPhoto(chatId, film.picture, {
+			caption: caption,
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{
+							text: 'Добавить в избранное',
+							callback_data: film.uuid
+						},
+						{
+							text: 'Показать кинотеатры',
+							callback_data: film.uuid
+						}
+					],
+					[
+						{
+							text: `Кинопоиск ${film.name}`,
+							url: film.link
+						}
+					]
+
+				]
+			}
+		})
+	})
+});
+
 // ===============================
 function sendFilmsByQuery(chatId, query) {
 	Film.find(query).then(films => {
